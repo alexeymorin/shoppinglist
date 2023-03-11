f8fcbc893eccf5bb799be87ee9c2df0078fb871d
morin <alexeymorin@gmail.com>
2023-03-11 11:48:37 +0300

    Добавил основную разметку.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
   return (
     <Container>
-      
+      <AppBar>
+        <Toolbar>
+          <Typography variant='h6'>
+            Shopping List
+          </Typography>
+        </Toolbar>    
+      </AppBar>
     </Container>
   );
 }
