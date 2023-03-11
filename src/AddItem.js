d61347152c6549be3189931ef18dbef6d2dc8be7
morin <alexeymorin@gmail.com>
2023-03-11 20:02:49 +0300

    Реализовал компонент AddItem.

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function AddItem(props) {
+  const [open, setOpen] = useState(false);
+  const [item, setItem] = useState({
+    product: '',
+    amount: ''
+  });
+
+  const handleOpen = () => {
+    setOpen(true);    
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    setItem({...item, [e.target.name]: e.target.value});
+  };
+
+  const addItem = () => {
+    props.addItem(item);
+    setItem({product: '', amount: ''}); // Очищаем поля
+    handleClose();
+  };
+
   return (
-    <div></div>
+    <div>
+      <Button onClick={handleOpen}>Add Item</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>New Item</DialogTitle>
+        <DialogContent>
+          <TextField value={item.product} margin='dense' onChange={handleChange} name='product' label='Product' fullWidth />
+          <TextField value={item.amount} margin='dense' onChange={handleChange} name='amount' label='Amount' fullWidth />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={AddItem}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
 
